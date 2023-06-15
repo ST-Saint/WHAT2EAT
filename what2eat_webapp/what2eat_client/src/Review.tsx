b01aafa7aab879969aa134513ddf279f8ed0ee66
Yayu Wang <st.saint.wyy@gmail.com>
2023-06-15 14:29:44 -0700

    update: use mui & autocomplete form

--- what2eat_webapp/what2eat_client/src/Review.tsx
+++ what2eat_webapp/what2eat_client/src/Review.tsx
@@ -1,153 +1,218 @@
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import "./review.scss";
-import { v4 as UUID } from "uuid";
+import './review.scss';
+import { css } from '@emotion/css';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import { v4 as UUID } from 'uuid';
 
-type reviewForm = {
-  reviewer: string;
-  restaurant: string;
-  dish: string;
-  score: number;
-  comment: string;
-  date: Date;
-};
+interface reviewForm {
+    reviewer: string;
+    restaurant: string;
+    dish: string;
+    score: number;
+    comment: string;
+    date: Date;
+}
 
 interface reviewState {
-  reviewer: string;
-  restaurant: string;
-  dish: string;
-  score: number;
-  comment: string;
-  date: Date;
+    reviewer: string;
+    restaurant: string;
+    dish: string;
+    score: number;
+    comment: string;
+    date: Date;
 }
 
 interface reviewProps {}
 
 const Review = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<reviewForm>();
-  const onSubmit: SubmitHandler<reviewForm> = async (review) => {
-    review.date = new Date();
-    let jsonRPCBody: any = {
-      jsonrpc: "2.0",
-      method: "add_review",
-      params: { review: review },
-      id: UUID(),
-    };
-    console.log(jsonRPCBody);
-    const response = await fetch("http://localhost:8080", {
-      method: "POST",
-      mode: "no-cors",
-      body: JSON.stringify(jsonRPCBody),
-      headers: { "Content-Type": "application/json; charset=UTF-8" },
-    });
+    const {
+        register,
+        control,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<reviewForm>();
 
-    if (!response.ok) {
-      /* Handle */
-    }
+    const reviwers: string[] = [];
+    const restaurants: string[] = [];
+    const dishes: string[] = [];
 
-    console.log(response.body);
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-
-      <label>Reviewer</label>
-      <input
-        placeholder="reviwer"
-        {...register("reviewer", { required: true })}
-      />
-      <label>Restaurant</label>
-      <input
-        placeholder="restaurant"
-        {...register("restaurant", { required: true })}
-      />
-      <label>dish</label>
-      <input placeholder="dish" {...register("dish")} />
-      <label>score</label>
-      <input
-        placeholder="score"
-        {...register("score", {
-          valueAsNumber: true,
-          validate: (value) => {
-            return value <= 100;
-          },
-          required: true,
-        })}
-      />
-      <label>comment</label>
-      <input
-        placeholder="comment"
-        {...register("comment")}
-      />
-
-      {errors.reviewer && <p>This field is required</p>}
-
-      <input type="submit" />
-    </form>
-  );
-};
-
-class review extends React.Component<reviewProps, reviewState> {
-  constructor(props: reviewProps) {
-    super(props);
-    this.state = {
-      reviewer: "",
-      restaurant: "",
-      dish: "",
-      score: 0,
-      comment: "",
-      date: new Date(),
+    const onSubmit: SubmitHandler<reviewForm> = async (review) => {
+        review.date = new Date();
+        let jsonRPCBody: any = {
+            jsonrpc: '2.0',
+            method: 'add_review',
+            params: { review: review },
+            id: UUID(),
+        };
+        console.log(review);
+        let response;
+        try {
+            response = await fetch('http://localhost:8080', {
+                method: 'POST',
+                mode: 'no-cors',
+                body: JSON.stringify(jsonRPCBody),
+                headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+            });
+            if (response != null && !response.ok) {
+                /* Handle */
+            }
+            console.log(response.body);
+        } catch (error) {}
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  // typing on RIGHT hand side of =
-  onChange = (e: React.FormEvent<HTMLInputElement>): void => {
-    this.setState({ reviewer: e.currentTarget.value });
-  };
-
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ reviewer: event.target.value });
-  }
-
-  handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
-    alert("A name was submitted: " + this.state);
-    event.preventDefault();
-  }
-
-  render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.state.reviewer}
-          onChange={this.onChange}
-        />
-      </div>
-    );
-  }
-}
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={css`
+                display: flex;
+                justify-content: center;
+                margin-top: 5em;
+            `}
+        >
+            <Stack spacing={2} sx={{ width: 300 }}>
+                {/* register your input into the hook by invoking the "register" function */}
 
-// const Review = () => {
-//   return (
-//     <div className="review">
-//       <form>
-//         <label>
-//           Name:
-//           <input type="text" name="name" />
-//         </label>
-//         <input type="submit" value="Submit" />
-//       </form>
-//     </div>
-//   );
-// };
+                <Controller
+                    name='reviewer'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                        <Autocomplete
+                            freeSolo
+                            onChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            onInputChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            disableClearable
+                            options={reviwers.map((reviewer) => reviewer)}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label='Reviewer'
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        type: 'search',
+                                    }}
+                                />
+                            )}
+                        />
+                    )}
+                />
+
+                <Controller
+                    name='restaurant'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                        <Autocomplete
+                            freeSolo
+                            onChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            onInputChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            disableClearable
+                            options={restaurants.map(
+                                (restaurant) => restaurant,
+                            )}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label='Restaurant'
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        type: 'search',
+                                    }}
+                                />
+                            )}
+                        />
+                    )}
+                />
+
+                <Controller
+                    name='dish'
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
+                        <Autocomplete
+                            freeSolo
+                            onChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            onInputChange={(event, value) => {
+                                onChange(value);
+                            }}
+                            disableClearable
+                            options={dishes.map((dish) => dish)}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label='Dish'
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        type: 'search',
+                                    }}
+                                />
+                            )}
+                        />
+                    )}
+                />
+
+                <Controller
+                    name='score'
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={0}
+                    render={({ field: { onChange, value } }) => (
+                        <TextField
+                            label='Score'
+                            type='number'
+                            defaultValue={0}
+                            error={isNaN(value) || value > 100}
+                            helperText={
+                                isNaN(value) || value > 100
+                                    ? 'Score must be <= 100'
+                                    : ''
+                            }
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>,
+                            ) => {
+                                onChange(event.target.value);
+                            }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    )}
+                />
+
+                <TextField
+                    label='Comment'
+                    multiline
+                    maxRows={5}
+                    {...register('comment')}
+                />
+                {errors.reviewer && <p>Reviewer is required</p>}
+
+                <Button type='submit' variant='contained'>
+                    Submit
+                </Button>
+            </Stack>
+        </form>
+    );
+};
 
 export default Review;
--- what2eat_webapp/what2eat_client/src/index.tsx
+++ what2eat_webapp/what2eat_client/src/index.tsx
@@ -2,37 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Review from "./Review";
+import Review from './Review';
 import reportWebVitals from './reportWebVitals';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Example from './example';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  {
-    path: "/helloworld",
-    element: <div>Hello world!</div>,
-  },
     {
-        path: "/",
+        path: '/helloworld',
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: '/',
         element: <App />,
     },
     {
-        path: "review",
+        path: 'review',
         element: <Review />,
-    }
+    },
+    {
+        path: 'example',
+        element: <Example />,
+    },
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement,
 );
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
-  </React.StrictMode>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
