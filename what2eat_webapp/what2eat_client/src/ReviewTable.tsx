46177e44be782e09f83788ad773358b46144c614
Yayu Wang <st.saint.wyy@gmail.com>
2023-07-30 15:04:00 -0700

    update: review uuid & paginator

--- what2eat_webapp/what2eat_client/src/ReviewTable.tsx
+++ what2eat_webapp/what2eat_client/src/ReviewTable.tsx
@@ -1,16 +1,25 @@
 import NavigationBar from './NavigationBar';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { useTheme } from '@mui/material/styles';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { v4 as UUID } from 'uuid';
 
 interface Data {
+    uuid: string;
     reviewer: string;
     restaurant: string;
     score: number;
@@ -18,9 +27,96 @@ interface Data {
     date: string;
 }
 
+interface TablePaginationActionsProps {
+    count: number;
+    page: number;
+    rowsPerPage: number;
+    onPageChange: (
+        event: React.MouseEvent<HTMLButtonElement>,
+        newPage: number,
+    ) => void;
+}
+function TablePaginationActions(props: TablePaginationActionsProps) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+
+    const handleFirstPageButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label='first page'
+            >
+                {theme.direction === 'rtl' ? (
+                    <LastPageIcon />
+                ) : (
+                    <FirstPageIcon />
+                )}
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label='previous page'
+            >
+                {theme.direction === 'rtl' ? (
+                    <KeyboardArrowRight />
+                ) : (
+                    <KeyboardArrowLeft />
+                )}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label='next page'
+            >
+                {theme.direction === 'rtl' ? (
+                    <KeyboardArrowLeft />
+                ) : (
+                    <KeyboardArrowRight />
+                )}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label='last page'
+            >
+                {theme.direction === 'rtl' ? (
+                    <FirstPageIcon />
+                ) : (
+                    <LastPageIcon />
+                )}
+            </IconButton>
+        </Box>
+    );
+}
+
 const ReviewTable = () => {
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     let [reviews, setReviews] = useState([]);
 
     useEffect(() => {
@@ -42,12 +138,32 @@ const ReviewTable = () => {
                 headers: { 'Content-Type': 'application/json; charset=UTF-8' },
             });
             let result = (await resp.json()).result;
-            console.log(result);
             setReviews(result);
         } catch (error) {
             console.log(error);
         }
     };
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - reviews.length) : 0;
+
+    const visibleRows = React.useMemo(() => {
+        return reviews.slice(
+            page * rowsPerPage,
+            page * rowsPerPage + rowsPerPage,
+        );
+    }, [page, rowsPerPage, reviews]);
 
     return (
         <>
@@ -64,9 +180,9 @@ const ReviewTable = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {reviews.map((review: Data) => (
+                        {visibleRows.map((review: Data) => (
                             <TableRow
-                                key={review.restaurant}
+                                key={review.uuid}
                                 sx={{
                                     '&:last-child td, &:last-child th': {
                                         border: 0,
@@ -94,8 +210,28 @@ const ReviewTable = () => {
                                 </TableCell>
                             </TableRow>
                         ))}
+                        {emptyRows > 0 && (
+                            <TableRow
+                                style={{
+                                    height: 53 * emptyRows,
+                                }}
+                            >
+                                <TableCell colSpan={5} />
+                            </TableRow>
+                        )}
                     </TableBody>
                 </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10]}
+                    colSpan={3}
+                    component='div'
+                    count={reviews.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActions}
+                />
             </TableContainer>
         </>
     );
