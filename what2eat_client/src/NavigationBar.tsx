821e9302166e7b1e5754b053c829c0f33207e396
Yayu Wang <st.saint.wyy@gmail.com>
2023-12-21 21:49:24 -0800

    update: mobile nav bar

--- what2eat_client/src/NavigationBar.tsx
+++ what2eat_client/src/NavigationBar.tsx
@@ -1,26 +1,137 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/Row';
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 
-const navItems = ['Home', 'Review', 'restaurant'];
+const pages: string[] = ['Home', 'Reviews', 'Edits'];
+const ref: string[] = [
+    '/',
+    '/reviewtable',
+    '/revieweditor',
+];
 
-const NavigationBar = () => {
+function NavigationBar() {
+    const [anchorElNav, setAnchorElNav] =
+        React.useState<null | HTMLElement>(null);
+    const handleOpenNavMenu = (
+        event: React.MouseEvent<HTMLElement>,
+    ) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+    const handleAnchor = (page: any, index: any) => {
+        window.location.href = ref[index];
+    };
+
     return (
-        <Navbar bg='light' expand='lg'>
-            <Container>
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link href='/'>Home</Nav.Link>
-                        <Nav.Link href='/reviewtable'>ReviewTable</Nav.Link>
-                        <Nav.Link href='/revieweditor'>ReviewEditor</Nav.Link>
-                        <Nav.Link href='/expenseeditor'>ExpenseEditor</Nav.Link>
-                        {/* TODO(table display): add Nav.Link to table*/}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <AppBar position='static'>
+                <Container maxWidth='xl'>
+                    <Toolbar disableGutters>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {
+                                    xs: 'flex',
+                                    md: 'none',
+                                },
+                            }}
+                        >
+                            <IconButton
+                                size='large'
+                                aria-label='account of current user'
+                                aria-controls='menu-appbar'
+                                aria-haspopup='true'
+                                onClick={handleOpenNavMenu}
+                                color='inherit'
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id='menu-appbar'
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: {
+                                        xs: 'block',
+                                        md: 'none',
+                                    },
+                                }}
+                            >
+                                {pages.map(
+                                    (page, index) => (
+                                        <MenuItem
+                                            key={page}
+                                            onClick={(
+                                                event,
+                                            ) => {
+                                                handleAnchor(
+                                                    page,
+                                                    index,
+                                                );
+                                            }}
+                                        >
+                                            <Typography textAlign='center'>
+                                                {page}
+                                            </Typography>
+                                        </MenuItem>
+                                    ),
+                                )}
+                            </Menu>
+                        </Box>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {
+                                    xs: 'none',
+                                    md: 'flex',
+                                },
+                            }}
+                        >
+                            {pages.map((page, index) => (
+                                <Button
+                                    key={page}
+                                    onClick={(event) => {
+                                        handleAnchor(
+                                            page,
+                                            index,
+                                        );
+                                    }}
+                                    sx={{
+                                        my: 2,
+                                        color: 'white',
+                                        display: 'block',
+                                    }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
-};
+}
+
 export default NavigationBar;
